c4bc4c58e5c0f9dcb7643239ba7eeb671bd56aa6
Egbert Nierop <egbert.nierop@adccure.nl>
2023-11-05 15:32:56 +0100

    Initial commit

---
+++ src/Examples/Program.cs
@@ -0,0 +1,18 @@
+﻿using NCV.SharpMagic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using LibMagic dll = new();
+        using var mp4 = File.OpenRead("highway.mp4");
+        var mp4info = dll.GuessMimeType(mp4, 2048, true);
+        Console.WriteLine($"detected mimetype = {mp4info.MimeType}");
+
+        using var jpg = File.OpenRead("gun.jpg");
+        var jpgInfo = dll.GuessMimeType(jpg, mimeOnly: true);
+        Console.WriteLine($"detected mimetype = {jpgInfo.MimeType}");
+
+        Console.WriteLine($"detected mimetype = {string.Join(',', dll.GuessMimeType(Path.GetFullPath("gun.jpg")).Properties)}");
+        Console.WriteLine($"detected mimetype = {string.Join(',', dll.GuessMimeType(Path.GetFullPath("highway.mp4")).Properties)}");
+    }
+}
